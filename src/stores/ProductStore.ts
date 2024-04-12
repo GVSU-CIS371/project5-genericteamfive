4cb3d08daab03e3e34828e3410e4e65bf38be2ee
ShawnC24 <69652935+ShawnC24@users.noreply.github.com>
2024-04-12 11:13:32 -0400

    Add files via upload

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -3,21 +3,20 @@ import { ProductDoc } from "../types/product";
 import { initProducts } from "../data-init";
 
 export const useProductStore = defineStore("ProductStore", {
-  state: () => ({
-    products: [] as ProductDoc[],
+    state: () => ({
+        products: [] as ProductDoc[],
   }),
-  actions: {
-    init() {
-      // Initialize the products with data from data-init.ts
-      this.products = initProducts;
-    },
-    filterByCategory(category: string) {
-      // Filter products by category
-      return this.products.filter(product => product.data.category === category);
-    },
-    filterByRating(minRating: number) {
-      // Filter products with ratings above minRating
-      return this.products.filter(product => product.data.rating >= minRating);
-    },
-  },
+    actions: {
+      init() {
+        this.products = initProducts;
+      },
+      filterByCategory(category: string) {
+        return this.products.filter(
+          product => product.data.category === category);
+      },
+      filterByRating(minRating: number){
+        return this.products.filter(
+          product => product.data.rating >= minRating);
+      }
+    }    
 });
